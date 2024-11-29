b8f28aa75c6956863baf41a8ad4dba1b1e0e91be
Dxniel06 <daniramirezoso1@gmail.com>
2024-11-29 02:24:15 -0600

    creacion de la rama 2

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ console.log("Primer commit")
 console.log("estoy en la trama del main")
 console.log("Hello Wordl") 
 console.log("rama del programador 1")
- 
+console.log("rama del programador 2")
+console.log("no pase quimica")
+
+
